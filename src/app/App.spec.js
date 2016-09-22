4bf53359462c873196b445dda00586115530e6a2
Xiaofan Wu <Xiaofan2406@gmail.com>
2016-09-22 13:32:53 +1000

    update test

--- src/app/App.spec.js
+++ src/app/App.spec.js
@@ -1,10 +1,12 @@
-/* global it, expect */
+/* global it, expect, describe, afterAll, beforeAll, afterEach, beforeEach */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders a header with img and title', () => {
-  const component = renderer.create(<App />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe('App', () => {
+  it('matches snapshots', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
