629b3de34a7eda754ec505515f6eb483473dee6c
Xiaofan Wu <Xiaofan2406@gmail.com>
2016-09-26 09:51:19 +1000

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,10 @@ module.exports = {
     node: true
   },
   settings: {
+    'import/ignore': [
+      'node_modules',
+      '\\.(json|css|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$',
+    ],
     'import/resolver': {
       webpack: {
         config: './config/webpack.dev.js'
