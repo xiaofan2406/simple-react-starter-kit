c5fc255ae0bf71f05f273aab938531f2ef653e33
Xiaofan Wu <xiaofan2406@gmail.com>
2017-05-27 10:11:29 +1000

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,13 +31,22 @@ module.exports = {
   plugins: ['react'],
   extends: ['airbnb'],
   rules: {
-    // additional rules to work with eslint-config-airbnb
-    'comma-dangle': [2, 'never'], // yuk, disallow comma after the last propery of an object
     'new-cap': 0, // disable for HigherOrderComponent wrapping
-    'no-console': 0, // still good for debuging
-    'no-underscore-dangle': 0, // _whatever is allowed
-    'no-param-reassign': 0, // e.target.value = ''; happens all the time
-    'no-plusplus': 0, // i++ is allowed
+    'no-console': 0, // allow console.log etc
+    'no-underscore-dangle': 0, // allow _func
+    'no-param-reassign': 0, // e.target.value = '';
+    'no-plusplus': 0, // allow i++
+    'no-confusing-arrow': 0,
+    'no-mixed-operators': 0,
+    indent: 0,
+    'react/jsx-indent': 0,
+    'react/jsx-indent-props': 0,
+    'react/jsx-closing-bracket-location': 0, // prettier takes care of indenting
+    'jsx-a11y/no-static-element-interactions': 0, // allow div onClick
+    'import/no-extraneous-dependencies': 0, // allow usage of devDependencies
+    'react/forbid-prop-types': 0, // allow PropTypes.object
+    'react/jsx-filename-extension': 0, // enfore all .js extension
+    'comma-dangle': [2, 'never'], // disallow trailing comma
     'no-restricted-syntax': [
       2,
       'ForInStatement',
@@ -46,10 +55,6 @@ module.exports = {
     ],
     'no-unused-vars': [2, { ignoreRestSiblings: true }],
     'arrow-parens': [2, 'as-needed'],
-    'jsx-a11y/no-static-element-interactions': 0, // div onClick should be allowed
-    'import/no-extraneous-dependencies': 0, // not necessary at all
-    'react/jsx-filename-extension': 0, // enfore all .js extension
-    'react/no-unused-prop-types': [2, { skipShapeProps: true }], // skip shape
-    'react/forbid-prop-types': 0 // PropTypes.object is allowed
+    'react/no-unused-prop-types': [2, { skipShapeProps: true }] // skip shape
   }
 };
