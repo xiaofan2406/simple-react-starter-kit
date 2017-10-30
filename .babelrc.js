a69da336e85be41e3e89fe42b81db7e23007db55
Xiaofan Wu <xiaofan2406@gmail.com>
2017-10-30 20:05:06 +1100

    babe plugins update

--- .babelrc.js
+++ .babelrc.js
@@ -3,10 +3,6 @@ let plugins = [
   'babel-plugin-transform-class-properties',
   ['babel-plugin-transform-object-rest-spread', { useBuiltIns: true }],
   ['babel-plugin-transform-react-jsx', { useBuiltIns: true }],
-  [
-    'babel-plugin-transform-runtime',
-    { helpers: false, polyfill: false, regenerator: true }
-  ],
   'babel-plugin-transform-export-extensions'
 ];
 
@@ -35,14 +31,23 @@ if (process.env.NODE_ENV === 'test') {
     presets: [
       [
         'babel-preset-env',
-        { targets: { uglify: true }, useBuiltIns: false, modules: false }
+        {
+          targets: {
+            browsers: [
+              'Chrome >= 60',
+              'Safari >= 10.1',
+              'iOS >= 10.3',
+              'Firefox >= 54',
+              'Edge >= 15'
+            ],
+            uglify: true
+          },
+          useBuiltIns: false,
+          modules: false
+        }
       ],
       'babel-preset-react'
     ],
-    plugins: [
-      ...plugins,
-      ['babel-plugin-transform-regenerator', { async: false }],
-      'babel-plugin-syntax-dynamic-import'
-    ]
+    plugins: [...plugins, 'babel-plugin-syntax-dynamic-import']
   };
 }
