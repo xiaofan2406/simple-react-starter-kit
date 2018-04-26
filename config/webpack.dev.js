6953fdefb241054d91cec550de2021db3a172947
Xiaofan Wu <xiaofan2406@gmail.com>
2018-04-26 20:13:58 +1000

    deps

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -61,7 +61,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"development"',
     }),
-    new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
   node: common.node,
