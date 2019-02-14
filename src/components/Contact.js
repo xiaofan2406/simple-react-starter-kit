1206ea0ae7a0f41a9c896ca3ed8f4f3c19539440
Xiaofan Wu <xiaofan2406@gmail.com>
2019-02-14 15:23:51 +1100

    fix: use css with memo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/core';
 import { Loader } from 'widgets';
 
-const Contact = () => (
-  <div
-    css={css`
-      text-align: center;
-    `}
-  >
-    <Loader size={120} />
-  </div>
-);
+const Contact = () => {
+  const [count, setCount] = useState(120);
+  const add = () => setCount(prev => prev + 10);
+
+  return (
+    <div
+      css={css`
+        text-align: center;
+      `}
+    >
+      <Loader size={count} />
+      <button type="button" onClick={add}>
+        Add
+      </button>
+    </div>
+  );
+};
 
 export default Contact;
--- src/widgets/Loader.js
+++ src/widgets/Loader.js
@@ -1,51 +1,60 @@
 /**
  * @see https://github.com/yuanyan/halogen/blob/master/src/RingLoader.js
  */
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 
-const Loader = ({ size, color }) => (
-  <div
-    css={css`
-      display: inline-block;
-    `}
-  >
-    <div
-      css={css`
+const Loader = ({ size, color }) => {
+  const cssSize = useMemo(
+    () =>
+      css`
         width: ${size}px;
         height: ${size}px;
-        position: relative;
+      `,
+    [size]
+  );
 
-        & div {
-          width: ${size}px;
-          height: ${size}px;
-          border-style: solid;
-          border-width: ${size / 10}px;
-          border-color: ${color};
-          opacity: 0.4;
-          border-radius: 100%;
-          position: absolute;
-          top: 0;
-          left: 0;
-          perspective: 800px;
-          animation-fill-mode: forwards;
-        }
+  return (
+    <div
+      css={css`
+        display: inline-block;
       `}
     >
       <div
         css={css`
-          animation: loaderRigthRotate 2s 0s infinite linear;
+          ${cssSize};
+          position: relative;
+
+          & div {
+            ${cssSize};
+            border-style: solid;
+            border-width: ${size / 10}px;
+            border-color: ${color};
+            opacity: 0.4;
+            border-radius: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            perspective: 800px;
+            animation-fill-mode: forwards;
+          }
         `}
-      />
-      <div
-        css={css`
-          animation: loaderLeftRotate 2s 0s infinite linear;
-        `}
-      />
+      >
+        <div
+          css={css`
+            animation: loaderRigthRotate 2s 0s infinite linear;
+          `}
+        />
+        <div
+          css={css`
+            animation: loaderLeftRotate 2s 0s infinite linear;
+          `}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Loader.propTypes = {
   color: PropTypes.string,
